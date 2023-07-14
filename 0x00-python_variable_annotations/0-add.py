c4df47afa56e040ab6cb76046631fb5dc7417128
DaffiX <suleydaffy@gmail.com>
2023-07-14 18:28:19 +0000

    Basic Annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+    Basic Annotation - Add
+'''
+
+
+def add(a: float, b: float) -> float:
+    ''' A function that takes float and return sum as float '''
+    return a + b
